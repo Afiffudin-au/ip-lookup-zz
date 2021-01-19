0ed8d8a7d0057f43b6625347bceebe6afc6e227b
afiffudin-au <afiffudin077@gmail.com>
2021-01-19 18:32:28 +0700

    LookupResult.js is finished

--- src/components/LookupResult/LookupResult.js
+++ src/components/LookupResult/LookupResult.js
@@ -1,13 +1,89 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectLookupIpBlocks } from '../../features/ipAddrSlice'
-
+import './LookupResult.scss'
 function LookupResult() {
   const {lookupIpResults} = useSelector(selectLookupIpBlocks)
   console.log(lookupIpResults)
   return (
-    <div>
-      
+    <div className="LookupResult">
+      {
+        lookupIpResults.length !== 0 &&  
+      <table>
+        <tbody>
+          <tr>
+            <td>city</td>
+            <td>{lookupIpResults.city}</td>
+          </tr>
+          <tr>
+            <td>continent code</td>
+            <td>{lookupIpResults.continent_code}</td>
+          </tr>
+          <tr>
+            <td>continent name</td>
+            <td>{lookupIpResults.continent_name}</td>
+          </tr>
+          <tr>
+            <td>country code</td>
+            <td>{lookupIpResults.country_code}</td>
+          </tr>
+          <tr>
+            <td>country name</td>
+            <td>{lookupIpResults.country_name}</td>
+          </tr>
+          <tr>
+            <td>ip</td>
+            <td>{lookupIpResults.ip}</td>
+          </tr>
+          <tr>
+            <td>latitude</td>
+            <td>{lookupIpResults.latitude}</td>
+          </tr>
+          <tr>
+            <td>city</td>
+            <td>{lookupIpResults.city}</td>
+          </tr>
+          <tr>
+            <td>capital</td>
+            <td>{lookupIpResults.location.capital}
+            <img className="flag" src={lookupIpResults.location.country_flag} alt=""/>
+            </td>
+          </tr>
+          <tr>
+            <td>languages</td>
+            {
+            lookupIpResults?.location?.languages?.map(item=>(
+                <td>
+                {
+                  item.name
+                }
+                </td>
+              ))
+            }
+          </tr>
+          <tr>
+            <td>longitude</td>
+            <td>{lookupIpResults.longitude}</td>
+          </tr>
+          <tr>
+            <td>region code</td>
+            <td>{lookupIpResults.region_code}</td>
+          </tr>
+          <tr>
+            <td>region name</td>
+            <td>{lookupIpResults.region_name}</td>
+          </tr>
+          <tr>
+            <td>type</td>
+            <td>{lookupIpResults.type}</td>
+          </tr>
+          <tr>
+            <td>zip</td>
+            <td>{lookupIpResults.zip}</td>
+          </tr>
+        </tbody>
+      </table>
+      }
     </div>
   )
 }
